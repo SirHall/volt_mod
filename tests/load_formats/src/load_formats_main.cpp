77d485299b330a87c8bdc431c6eba7c7c312e2e2
SirHall <sirhallstein@gmail.com>
2020-01-14 00:20:34 +1000

    Add support for printing entire autoload json files to load_formats test

--- tests/load_formats/src/load_formats_main.cpp
+++ tests/load_formats/src/load_formats_main.cpp
@@ -20,10 +20,18 @@ int main(int argc, char *argv[])
                   << "Author:\t\t" << module.GetAuthor() << "\n\t"
                   << "Version:\t" << module.GetVersion() << "\n\t"
                   << "Site:\t\t" << module.GetSite() << "\n\t"
-                  << "JSONs:\t\t";
+                  << "JSONs:\n";
         for (auto &jsonFile : module.GetAutoloadJSONs())
-            std::cout << jsonFile.GetName() << " ";
-        std::cout << "\n}" << std::endl;
+        {
+            std::cout << "\t\t{\n";
+            for (auto &jsonField : jsonFile.GetRoot())
+                std::cout << "\t\t\t" << jsonField.first << ": "
+                          << jsonField.second.get_value_optional<std::string>()
+                                 .value_or("")
+                          << "\n";
+            std::cout << "\t\t}\n";
+        }
+        std::cout << "}" << std::endl;
     }
 
     return 0;
