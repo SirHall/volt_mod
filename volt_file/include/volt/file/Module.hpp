c63e5f1002eb6206656fe102cff90f54eacf3bd4
SirHall <sirhallstein@gmail.com>
2020-01-25 14:33:46 +1000

    Deconst Module fields to allow move/copy assignment operator overloads, and const correct member functions

--- volt_file/include/volt/file/Module.hpp
+++ volt_file/include/volt/file/Module.hpp
@@ -5,61 +5,88 @@
 #include "volt/file/JSONData.hpp"
 
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace volt::file
 {
+
+    enum ModuleOrganizationStatus
+    {
+        Successful,
+        MissingDependency,
+        DependencyLoop
+    };
+
     class Module
     {
     private:
-        const std::vector<JSONData>    jsonObjs;
-        const std::string              moduleName;
-        const std::vector<std::string> dependencies;
-        const std::vector<std::string> loadBefore;
-        const std::vector<std::string> loadAfter;
-        const std::string              version;
-        const std::string              softwareVersion;
-        const std::string              author;
-        const std::string              site;
-        const std::string              repository;
+        std::vector<JSONData>    jsonObjs;
+        std::string              moduleName;
+        std::vector<std::string> dependencies;
+        // std::vector<std::string> loadBefore;
+        std::vector<std::string> loadAfter;
+        std::string              version;
+        std::string              softwareVersion;
+        std::string              author;
+        std::string              site;
+        std::string              repository;
 
-    protected:
     public:
         Module(std::vector<JSONData> jsonObjects, std::string name,
                std::vector<std::string> deps,
                std::vector<std::string> loadAfterModules,
-               std::vector<std::string> loadBeforeModules,
+               //    std::vector<std::string> loadBeforeModules,
                std::string moduleVersion, std::string targetSoftwareVersion,
                std::string authorName, std::string siteURL,
                std::string repositoryURL);
         Module(const Module &other);
+        Module &operator=(const Module &other);
         Module(Module &&other);
+        Module &operator=(Module &&other);
+
         ~Module();
 
-        inline std::vector<JSONData> const &GetAutoloadJSONs()
+        inline std::vector<JSONData> const &GetAutoloadJSONs() const
         {
             return this->jsonObjs;
         }
 
-        inline std::string const &GetModuleName() { return this->moduleName; }
+        inline std::string const &GetModuleName() const
+        {
+            return this->moduleName;
+        }
 
-        inline std::vector<std::string> const &GetDependencies()
+        inline std::vector<std::string> const &GetDependencies() const
         {
             return this->dependencies;
         }
 
-        inline std::string const &GetVersion() { return this->version; }
+        inline std::string const &GetVersion() const { return this->version; }
 
-        inline std::string const &GetTargetVersion()
+        inline std::string const &GetTargetVersion() const
         {
             return this->softwareVersion;
         }
 
-        inline std::string const &GetAuthor() { return this->author; }
+        inline std::string const &GetAuthor() const { return this->author; }
 
-        inline std::string const &GetSite() { return this->site; }
+        inline std::string const &GetSite() const { return this->site; }
 
-        inline std::string const &GetRepository() { return this->repository; }
+        inline std::string const &GetRepository() const
+        {
+            return this->repository;
+        }
+
+        inline std::vector<std::string> const &GetLoadAfter() const
+        {
+            return this->loadAfter;
+        }
+
+        // inline std::vector<std::string> const &GetLoadBefore() const
+        // {
+        //     return this->loadBefore;
+        // }
     };
 
 } // namespace volt::file
--- volt_file/src/volt/file/Module.cpp
+++ volt_file/src/volt/file/Module.cpp
@@ -1,16 +1,20 @@
 #include "volt/file/Module.hpp"
 
+#include <algorithm>
+#include <functional>
+
 using namespace volt::file;
 
 Module::Module(std::vector<JSONData> jsonObjects, std::string name,
                std::vector<std::string> deps,
                std::vector<std::string> loadAfterModules,
-               std::vector<std::string> loadBeforeModules,
+               //    std::vector<std::string> loadBeforeModules,
                std::string moduleVersion, std::string targetSoftwareVersion,
                std::string authorName, std::string siteURL,
                std::string repositoryURL)
     : jsonObjs(jsonObjects), moduleName(name), dependencies(deps),
-      loadAfter(loadAfterModules), loadBefore(loadBeforeModules),
+      loadAfter(loadAfterModules),
+      //   loadBefore(loadBeforeModules),
       version(moduleVersion), softwareVersion(targetSoftwareVersion),
       author(authorName), site(siteURL), repository(repositoryURL)
 {
@@ -18,24 +22,49 @@ Module::Module(std::vector<JSONData> jsonObjects, std::string name,
 
 Module::Module(const Module &other)
     : jsonObjs(other.jsonObjs), moduleName(other.moduleName),
-      loadAfter(other.loadAfter), loadBefore(other.loadBefore),
-      dependencies(other.dependencies), version(other.version),
-      softwareVersion(other.softwareVersion), author(other.author),
-      site(other.site), repository(other.repository)
+      loadAfter(other.loadAfter), dependencies(other.dependencies),
+      version(other.version), softwareVersion(other.softwareVersion),
+      author(other.author), site(other.site), repository(other.repository)
 {
+}
+
+Module &Module::operator=(const Module &other)
+{
+    jsonObjs        = other.jsonObjs;
+    moduleName      = other.moduleName;
+    loadAfter       = other.loadAfter;
+    dependencies    = other.dependencies;
+    version         = other.version;
+    softwareVersion = other.softwareVersion;
+    author          = other.author;
+    site            = other.site;
+    repository      = other.repository;
+    return *this;
 }
 
 Module::Module(Module &&other)
     : jsonObjs(std::move(other.jsonObjs)),
       moduleName(std::move(other.moduleName)),
       dependencies(std::move(other.dependencies)),
-      loadAfter(std::move(other.loadAfter)),
-      loadBefore(std::move(other.loadBefore)),
-      version(std::move(other.version)),
+      loadAfter(std::move(other.loadAfter)), version(std::move(other.version)),
       softwareVersion(std::move(other.softwareVersion)),
       author(std::move(other.author)), site(std::move(other.site)),
       repository(std::move(other.repository))
 {
+}
+
+Module &Module::operator=(Module &&other)
+{
+    jsonObjs        = std::move(other.jsonObjs);
+    moduleName      = std::move(other.moduleName);
+    loadAfter       = std::move(other.loadAfter);
+    dependencies    = std::move(other.dependencies);
+    version         = std::move(other.version);
+    softwareVersion = std::move(other.softwareVersion);
+    author          = std::move(other.author);
+    site            = std::move(other.site);
+    repository      = std::move(other.repository);
+    return *this;
 }
 
 Module::~Module() {}
